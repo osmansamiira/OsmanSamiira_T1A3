5018a9da75a676ef8bab2a43f9efe0181a6ebc64
osmansamiira <samiraosman16@gmail.com>
2022-12-13 16:23:05 +1100

    Changed the wishlist feature to deleted

--- ProgReadApp.py
+++ ProgReadApp.py
@@ -1,10 +1,11 @@
+Book = {}
+
 def print_menu():
     print ("""Choose a number:
     1. Add a book
     2. Update a book
     3. View all books
-    4. View wishlist
-    5. Delete a book
+    4. Delete a book
     """)
 
 
@@ -16,6 +17,9 @@ while(True):
     response = int(input())
     if response == 1:
         print("Add a book")
+        name = input("Enter the book name:")
+        progress = input("Enter number of pages read:")
+        Book[name] = progress
     elif response == 2:
         print("Updating a book")
     elif response == 3: 
