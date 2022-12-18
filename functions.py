3655cd5ccd2ccbbcb24879eb1a0da3d0e1ba788d
osmansamiira <samiraosman16@gmail.com>
2022-12-18 16:07:02 +1100

    added extra function to write into wishlist

--- functions.py
+++ functions.py
@@ -1,10 +1,18 @@
 Book = {}
+import csv
+from os import system
+from csv import writer 
+import pandas as pd
 
 # function to add book
 def add_book(): 
     name = input("Enter the book name: ")
     progress = input("Enter number of pages read: ")
+    print("Book has been successfully added")
+    print("Select 8 to return to menu")
     Book[name] = progress
+    
+
 
 # function to search for a book
 def search_book(): 
@@ -46,3 +54,36 @@ def delete_book():
             display_book()
     else: 
             print("Book is not found in the list")
+
+# function to view wishlist 
+def view_wishlist(): 
+     df= pd.read_csv('./amazonbookswishlist.csv')
+     print('')
+     print('*********************')
+     print('Books Amazon Wishlist')
+     print('*********************')
+     print(df.to_string (index=False))
+     print('')
+
+# function to add to Amazon wishlist 
+def NumberOfBooks(): 
+    int(input("Please enter how many books you wish to add: "))
+    
+def add_wishlist(): 
+     with open('amazonbookswishlist.csv', 'w+') as file: 
+        myfile = csv.writer(file)
+        myfile.writerow(["Book Name", "Amazon Link"])
+        option = NumberOfBooks()
+        for i in range (option): 
+           BookName = input("Please enter the name of the book: ")
+           AmazonLink = input("Please enter the Amazon link: ")
+           myfile.writerow([BookName, AmazonLink])
+       
+
+       
+
+        
+  
+
+
+    
