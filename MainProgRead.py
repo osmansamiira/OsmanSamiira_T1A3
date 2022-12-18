e9236ff46c3588a93f074158195d84f6426456ef
osmansamiira <samiraosman16@gmail.com>
2022-12-18 16:06:32 +1100

    fixed error handling

--- MainProgRead.py
+++ MainProgRead.py
@@ -1,42 +1,84 @@
 from os import system 
-import webbrowser
 import csv
+from csv import writer
 import pandas as pd
-from functions import add_book, search_book, display_book, update_book, delete_book
+from functions import add_book, search_book, display_book, update_book, delete_book, view_wishlist, add_wishlist
 Book = {}
 wishlist = {}
 
-# function to display the menu
+# function to display the menu options
 def print_menu():
-    print ("""Choose an option:
-    1. Add a book
-    2. Search for a book
-    3. View all books
-    4. Update a book
-    5. Delete a book
-    6. View Amazon wishlist
-    """)
+    print("1. Add a book")
+    print("2. Search for a book ")
+    print("3. Display all my books")
+    print("4. Update a book")
+    print("5. Delete a book")
+    print("6. Add to my wishlist")
+    print("7. View my wishlist")
+    print("8. Return to menu")
+    print("9. Exit app")
+    opt = input("Choose your option (1-8): ")
+    return opt
 
-print("Welcome to ProgRead, your personal reading tracker")
-while (True): 
-    option = print_menu()
-    #error handling if a number isn't chosen
-    try:
-        option = int(input())
-    except ValueError: 
-        print("Invalid option. Please enter a number between 1-6.")
-        option = int(input())
-    if option == 1:
+
+option = ""
+    
+print("- Welcome to ProgRead, your personal reading tracker -")
+while option != "8":
+    option = print_menu() 
+    if option == "1":
         print("Add a book")
         add_book()
-    elif option == 2:
+    elif option == "2":
         search_book()
-    elif option == 3:  
+    elif option == "3":  
         display_book()
-    elif option == 4: 
+    elif option == "4": 
         update_book()
-    elif option == 5:
+    elif option == "5":
         delete_book() 
-    elif option == 6: 
-         df= pd.read_csv('./wishlist.csv')
-         print(df.to_string()) 
+    elif option == "6": 
+        add_wishlist()
+    elif option == "7":
+        view_wishlist()
+    elif option == "8": 
+        continue
+    elif option == "9": 
+        break
+
+    # error handling if a number between 1-9 isn't selected
+
+    try:
+        option = int(input())
+        if (option == 1): 
+            add_book()
+        elif (option == 2): 
+            search_book()
+        elif (option == 3): 
+            display_book()
+        elif (option == 4): 
+            update_book()
+        elif (option == 5): 
+            delete_book()
+        elif (option == 6): 
+            add_wishlist()
+        elif (option == 7): 
+            view_wishlist()
+        elif (option == 8): 
+            continue 
+        elif (option == 9): 
+            break
+    except ValueError:
+        print("Invalid input. Please enter a number between 1-9.")
+   
+
+
+    
+        
+
+
+
+
+
+
+
